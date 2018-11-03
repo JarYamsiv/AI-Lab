642503062f9793bd0460e99d14e4d1331142ff6a
Vismay Raj <vismayraj1@gmail.com>
2018-11-04 00:12:48 +0530

    ucb1 done

--- ucb_algo/ucb.py
+++ ucb_algo/ucb.py
@@ -14,15 +14,22 @@ from copy import deepcopy
 N_expiriments = 100
 N_episodes = 500000
 
-def up():
+def up(x=1):
     # My terminal breaks if we don't flush after the escape-code
-    sys.stdout.write('\x1b[1A')
-    sys.stdout.flush()
+    for i in range(x):
+    	sys.stdout.write('\x1b[1A')
+    	sys.stdout.flush()
 
-def down():
+def down(x=1):
     # I could use '\x1b[1B' here, but newline is faster and easier
-    sys.stdout.write('\n')
-    sys.stdout.flush()
+    for i in range(x):
+    	sys.stdout.write('\n')
+    	sys.stdout.flush()
+
+def erase():
+	#print("\r",end='')
+	sys.stdout.write('\x1b[2K')
+	sys.stdout.flush()
 
 def expression(qa,na,t):
 	return 0
@@ -59,30 +66,26 @@ class Agent:
 
 		print("states",self.itr)
 		for i in range(self.N):
-			for j in range(printlen+15):
-				print(' ',end='')
-			print("\r",end='')
+			erase()
 
 			self.Cpos += 1
 			print("bandit",i,'[',end='')
 			for j in range(int(printlen*arr[i])):
 				print('=',end='')
 			print(']')
-		for i in range(self.N+1):
-			up()
+		up(self.N+1)
 
 	def updateQ(self,action,reward):
 		self.k[action] += 1
 		self.Q[action] += (1./self.k[action]) * (reward - self.Q[action])
 
-	def get_action(self,bandit,force_Explore=False):
-		rand = np.random.random()
-		if rand<self.epsilon or force_Explore:
-			action_explore = np.random.randint(bandit.N)
-			return action_explore
-		else:
-			action_greedy = np.random.choice(np.flatnonzero(self.Q == self.Q.max()))
-			return action_greedy
+	def get_ucb(self,bandit):
+		total_rounds = sum(self.k)
+		exp = [self.val(x,total_rounds) for x in range(len(self.Q))]
+		return np.argmax(exp)
+
+	def val(self,x,y):
+		return self.Q[x]+np.sqrt(2*np.log(y)/self.k[x])
 
 
 
@@ -90,8 +93,14 @@ if __name__ == "__main__":
 	print("Commencing program")
 	bandit = Bandit([0.10,0.50,0.60,0.80,0.10,0.25,0.60,0.45,0.75,0.65])
 	agent = Agent(bandit,0.01)
+
+	for j in range(bandit.N):
+		reward = bandit.get_reward(j)
+		agent.updateQ(j,reward)
+
+	
 	for j in range(N_episodes):
-		action = agent.get_action(bandit)
+		action = agent.get_ucb(bandit)
 		reward = bandit.get_reward(action)
 		agent.updateQ(action,reward)
 		agent.printStats()
